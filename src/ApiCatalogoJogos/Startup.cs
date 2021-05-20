c8549b67ab60bcd2d0e62500ed101a613e52d3e1
PauloAlbes8039 <pj38alves@gmail.com>
2021-05-19 23:02:29 -0300

    Implementação do ExceptionMiddleware

--- src/ApiCatalogoJogos/Startup.cs
+++ src/ApiCatalogoJogos/Startup.cs
@@ -1,21 +1,16 @@
 using ApiCatalogoJogos.Controllers.V1;
+using ApiCatalogoJogos.Middleware;
 using ApiCatalogoJogos.Repositories;
 using ApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace ApiCatalogoJogos
 {
@@ -60,6 +55,8 @@ namespace ApiCatalogoJogos
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCatalogoJogos v1"));
             }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
